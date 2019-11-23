d70339b2045a668199ba227b1552534b516a8cd4
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2019-11-23 11:09:23 -0800

    JS: Solved reverse int

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,6 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let sign = n < 0 ? -1 : 1;
+  n = Math.abs(n);
+  let rev = 0;
+
+  while (n / 10 !== 0) {
+    rev *= 10;
+    let digit = n % 10;
+    rev += digit;
+    n = Math.floor(n / 10);
+  }
+
+  return rev * sign;
+}
 
 module.exports = reverseInt;
