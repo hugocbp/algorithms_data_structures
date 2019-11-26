cd58cb63dca4ec9ca4db258171cdbebf1ba0d774
Hugo Carlos Borges Pinto <hugocbp@gmail.com>
2019-11-25 18:02:36 -0800

    JS: Solved maxChar

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let count = {};
+  for (let char of str) {
+    if (count[char]) count[char]++;
+    else count[char] = 1;
+  }
+
+  let maxChar = "";
+  let maxCharCount = 0;
+
+  for (let char in count) {
+    if (count[char] > maxCharCount) {
+      maxChar = char;
+      maxCharCount = count[char];
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
